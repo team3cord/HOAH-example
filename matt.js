92b7de296b5c8cbaf21b6a1a3a5bab031f55f416
MattCordeiro <matthew3cord@gmail.com>
2015-05-28 21:39:34 -0700

    Cleaned up js.

--- matt.js
+++ matt.js
@@ -1,9 +1,8 @@
 jQuery(document).ready(function(){
-   console.log("Im ready!");
+   console.log("Jquery is ready!");
    var menuBtn = jQuery('.hamburger');
       menuBtn.on('click', function(e){
       e.preventDefault();
          jQuery('#mainMenu').slideToggle('slow');
-            //addClass('open-mc');
    });
 });
